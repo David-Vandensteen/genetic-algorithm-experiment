f3d3c963b95d1b3b2bf8d78fbca59b4d4f2550ec
Nainain <dvandensteen@gmail.com>
2018-10-02 13:51:27 +0200

    mario bros agent test

--- lua/super_mario_bros-test.lua
+++ lua/super_mario_bros-test.lua
@@ -9,58 +9,128 @@
 local inspect = require "inspect"
 require "lua-extend"
 require "genetic"
+require "logger"
 
-function main()
+game = {}
+game.settings = {}
+--Speed Supported are "normal","turbo","nothrottle","maximum"
+game.settings.speed = {}
+game.settings.speed.value = "turbo"
+game.settings.speed.set = {}
+function game.settings.speed.set.maximum() end
+function game.settings.speed.set.turbo() end
+function game.settings.speed.set.normal() end
+game.settings.joypad = {}
+game.settings.joypad.rate = 40
+
+
+
+mario = {}
+emu = {}
+joypad ={}
+gui = {}
+
+function gui.text(x, y, msg) print(msg) end
+
+function joypad.write()
+  print("joypad.write()")
+end
+
+function emu.framecount()
+  print("emu.framecount()")
+  return 1
+end
+
+function emu.frameadvance()
+  io.read()
+end
+
+function mario.start()
+  print("mario.start()")
+end
+
+function mario.isDead() end
+function mario.getWorld() return 1 end
+function mario.getLevel() return 1 end
+function mario.getScore() return 1 end
+function mario.getPosition() return 1 end
+
+function mario.hud()
+  gui.text(0, 0, "generation")
+  gui.text(50, 0, genetic.generationIndex)
+  gui.text(0, 10, "genome")
+  gui.text(50, 10, genetic.genomeIndex)
+  gui.text(0, 40, "world")
+  gui.text(0, 50, "level")
+  gui.text(50, 40, mario.getWorld())
+  gui.text(50, 50, mario.getLevel())
+  gui.text(150, 0, "max. score")
+  --gui.text(210, 0, game.genetic.scores.max)
+  gui.text(150, 10, "cur. score")
+  gui.text(210, 10, mario.getScore())
+  gui.text(150, 40, "cur. position")
+  gui.text(210, 40, mario.getPosition())
+end
+
+function init()
+  print("init()")
+end
+
+function wait(frameMax)
+  local curF = emu.framecount()
+  while emu.framecount() < curF + frameMax do
+    emu.frameadvance()
+  end
+end
+
+function joypadUpdate(value)
+  if value == 0 then joypad.write(1, {A = false, right = true, left = false, down = false}) end
+  if value == 1 then joypad.write(1, {A = true, right = false, left = false, down = false}) end
+  if value == 2 then joypad.write(1, {A = false, right = false, left = false, down = true}) end
+  if value == 3 then joypad.write(1, {A = true, right = true, left = false, down = false}) end
+  if value == 4 then joypad.write(1, {A = true, right = true, left = false, down = true}) end
+end
+
+function main()  
   genetic.genomeMax = 10
   genetic:addGenome()
+  genetic:processGene()
+  logger.setFile("super_mario_bros.log")
+  logger.clear()
+  logger.info(os.date())
+  logger.info("")
+  local control = 0
+  init()
+  mario.start()
   while true do
-    print("------------------------------------- cycle start --")
-    genetic:processGene()
-    if math.random(0, 9) == 0 then
-      genetic:setScore(math.random(100, 10000))
-      genetic:addGenome()
+    if (emu.framecount() % game.settings.joypad.rate) == 0 then
+      control = genetic:processGene()
     end
-    if genetic:generationIsFinish() then
-      print("    ------ GENERATION END")      
-      genetic:sort()
-      print("scores")
-      print(inspect(genetic.scores))
-      print("")
-      print("clear genomes")
-      genetic:clearGenomes()
-      print("")
-      print("print genomes")
-      print(inspect(genetic.genomes))
-      print("")
-      print("copy the 2 bests genomes")
-      for j = 1, table.getn(genetic.scores) do
-        for i = 1, 2 do
-          if genetic.scores[j].ranking == i then
-            genetic.genomes[i] = table.copy(genetic.scores[j].genome)
+    joypadUpdate(control)
+    if mario.isDead() then
+      genetic:setScore(mario.getScore())
+      genetic:addGenome()
+      if genetic:generationIsFinish() then
+        updateLog()
+        genetic:clearGenomes()
+        -- copy the bests
+        for j = 1, table.getn(genetic.scores) do
+          for i = 1, 4 do
+            if genetic.scores[j].ranking == i then
+              genetic.genomes[i] = table.copy(genetic.scores[j].genome)
+              table.trunc(genetic.genomes[i], table.getn(genetic.genomes[i]) - 3)
+            end
           end
         end
+        genetic:addGeneration()
+        --control = genetic:processGene()
+        wait(50)
       end
-      print("")
-      print("print genomes")
-      print(inspect(genetic.genomes))
-      print("")
-      print("reset index")
-      genetic.genomeIndex = 0
-      genetic.geneIndex = 0
-      genetic.generationIndex = genetic.generationIndex + 1
-      genetic:addGenome()
+      emu.softreset()
+      mario.start()
     end
-    print("generation : ", genetic.generationIndex)
-    print("genome : ", genetic.genomeIndex)
-    print("gene :   ", genetic.geneIndex)
-    print("")
-    print(inspect(genetic.genomes))
-    print("")
-    print("")
-
-    print("")
-    print("-------------------------------------- cycle end --")
-    io.read()
+    mario.hud()
+    emu.frameadvance()
   end
 end
 
