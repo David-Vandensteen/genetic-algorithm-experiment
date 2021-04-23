a2004a2783d376562c387d24f2fccd9b6d8f28b3
David Vandensteen <david.vandensteen@leroymerlin.fr>
2021-04-23 10:21:01 +0200

    Add : bunyan

--- nodejs/hal-server/src/config.js
+++ nodejs/hal-server/src/config.js
@@ -1,25 +1,6 @@
-import pkg from '../package.json';
-
-const config = {
-  app: {
-    server: {
-      host: process.env.APP_SERVER_HOST || '0.0.0.0',
-      port: process.env.APP_SERVER_PORT || 8080,
-    },
-  },
-  health: {
-    server: {
-      host: process.env.HEALTH_SERVER_HOST || '0.0.0.0',
-      port: process.env.HEALTH_SERVER_PORT || 8081,
-    },
-  },
-  logger: {
-    config: {
-      name: pkg.name,
-      streams: [{ level: 'debug', stream: process.stdout }],
-    },
+export default {
+  server: {
+    host: process.env.HAL_SERVER_HOST || '0.0.0.0',
+    port: process.env.HAL_SERVER_PORT || 81,
   },
 };
-
-export default config;
-// export const { logger } = config;
--- nodejs/hal-server/src/lib/hal.js
+++ nodejs/hal-server/src/lib/hal.js
@@ -1,72 +1,49 @@
 import net from 'net';
+import bunyan from 'bunyan';
 import Operation from './operation';
 import Macro from './macro';
+import config from '../config';
 
-const { log, error } = console;
+const log = bunyan.createLogger({ name: 'hal-server' });
 
 export default class Hal {
   static start() {
-    const server = net.createServer(() => {
-      // socket.write('Echo server\n');
-      // socket.pipe(socket);
-    });
+    const server = net.createServer();
 
     server.on('connection', (socket) => {
-      log('client connected');
-      socket.on('data', (data) => {
-        log('client send :', data.toString());
-        // socket.write('Ok data received...\n');
-        // socket.pipe(socket);
-      });
-      /*
-      setInterval(() => {
-        const response = [
-          { operation: 'print', params: ['Hello'] },
-          { operation: 'emu.frameadvance' },
-        ];
-        socket.write(`${JSON.stringify(response)}\n`);
-        socket.pipe(socket);
-      }, 1);
-      */
-      const response = new Operation()
-        .add(Macro.init('normal'))
-        .add(Macro.start())
-        .add(Macro.joypadWriteRandom({
-          a: 0.5,
-          b: 0.5,
-          right: 0.5,
-          left: 0.5,
-          down: 0.5,
-          up: 0.5,
-        }, {
-          autoFrame: true,
-          quantity: 200,
-        }))
-        .commit();
+      log.info('client connected');
 
-      socket.write(`${JSON.stringify(response)}\n`);
-      socket.pipe(socket);
+      socket.on('data', (data) => {
+        log.info('client send :', data.toString());
+      });
+      Hal.send(Hal.response(), socket);
     });
 
     server.on('close', () => {
-      log('client disconnected');
+      log.info('client disconnected');
     });
 
     server.on('error', (err) => {
-      error(err);
+      log.error(err);
     });
 
     server.on('listening', () => {
-      log('server started');
+      log.info('server started');
     });
 
     server.on('data', (data) => {
-      log('client send :', data);
+      log.info('client send :', data);
     });
 
-    server.listen(81, '127.0.0.1');
+    server.listen(config.server.port, config.server.host);
+  }
 
-    /*
+  static send(data, socket) {
+    socket.write(`${JSON.stringify(data)}\n`);
+    socket.pipe(socket);
+  }
+
+  static response() {
     return new Operation()
       .add(Macro.init('normal'))
       .add(Macro.start())
@@ -82,6 +59,5 @@ export default class Hal {
         quantity: 200,
       }))
       .commit();
-      */
   }
 }
